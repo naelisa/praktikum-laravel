1603c4e53ca32680888e897c828e6712ac098418
naelisa <naelisaraz@email.com>
2026-03-11 02:38:04 +0700

    update tampilan html css

--- resources/views/mahasiswa/index.blade.php
+++ resources/views/mahasiswa/index.blade.php
@@ -1,6 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Data Mata Kuliah</title>
+    <style>
+        body{
+            font-family: Arial, sans-serif;
+            background-color: #f4f6f9;
+            margin: 40px;
+        }
+
+        h1{
+            text-align: center;
+            color: #333;
+        }
+
+        table{
+            border-collapse: collapse;
+            width: 70%;
+            margin: auto;
+            background: white;
+        }
+
+        th, td{
+            padding: 10px;
+            border: 1px solid #ccc;
+            text-align: center;
+        }
+
+        th{
+            background-color: #4CAF50;
+            color: white;
+        }
+
+        tr:nth-child(even){
+            background-color: #f2f2f2;
+        }
+
+        tr:hover{
+            background-color: #ddd;
+        }
+    </style>
+</head>
+
+<body>
+
 <h1>Data Mata Kuliah</h1>
 
-<table border="1">
+<table>
     <tr>
         <th>ID</th>
         <th>Kode</th>
@@ -18,3 +64,6 @@
     @endforeach
 
 </table>
+
+</body>
+</html>
